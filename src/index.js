3af10eafd3d82a67700f3c8d28fdf3218474630c
Uros Damnjanovic <uros.damnjanovic@gmail.com>
2018-11-30 22:54:24 +0200

    Cleaning up the code

--- src/index.js
+++ src/index.js
@@ -13,151 +13,6 @@ const getProvider = network => {
   return ethers.getDefaultProvider(network);
 }
 
-// There is a number of transactions per block that needs to be taken into consideration
-const getNumberOfBlocks = numberOfAddresses => {
-  if(numberOfAddresses <= estimatedNumberOfTransactionsPerBlock) {
-    return 1;
-  }
-
-  return Math.ceil(numberOfAddresses / estimatedNumberOfTransactionsPerBlock);
-};
-
-const mergeArrayToCollection = (arr, coll, propertyName) => {
-  
-  const updatedColl = coll.map((collEl, i) => {
-    return {...collEl, ...{[propertyName]: arr[i]}};
-  })
-  return updatedColl;
-}
-
-
-const getAddressesPerBlocks = (numberOfAddresses, numberOfBlocks) => {
-  const blocks = [];
-
-  for(let i = numberOfBlocks; i > 0; i-- ) {
-
-    const limit = numberOfAddresses  > numberOfBlocks - 1 ? (numberOfAddresses - numberOfBlocks + 1) : 1;
-    const numberOfAddressesInTheBlock = i > 1 ? Math.ceil(Math.random() * limit) : numberOfAddresses;
-    numberOfAddresses = numberOfAddresses - numberOfAddressesInTheBlock;
-
-    // For given blockIndex, check if we have enought transactions to get all the addresses. If not get another block.
-    blocks.push({
-      addressesPerBlock: numberOfAddressesInTheBlock
-    });
-  }
-  return [...blocks];
-}
-
-const getNRandomNumbers = (from, to, n) => {
-  const randomNumbers = [];
-  for(let i = 0; ; i < n) {
-    const rNumber = Math.random() * (to - from) + from;
-    if(randomNumber.indexOf(rNumber) === -1) {
-      randomNumbers.push(rNumber);
-      i++;
-    }
-  }
-}
-
-// TODO this should be more efficient and reliable
-const findValidBlock = async (numberOfTransactions, currentBlock) => {
-  return new Promise(async (resolve, reject) => {
-    let blockValid = false;
-    do{
-      const blockIndex = currentBlock - Math.floor(Math.random() * blocksTreshold);
-      try{
-        const txs = await provider.getBlock(blockIndex);
-        blockValid = txs.transactions.length > numberOfTransactions;
-        if(blockValid){
-          // For a given block we need to generate array of indexes for which we are going to 
-          // access transactions
-          //const txIndexes = getNRandomNumbers(0, txs.transactions.length, n);
-          //rxIndexes.map()
-          // Ok here we know we are good
-          resolve(blockIndex);
-        }
-      }catch(e) {
-        reject(e);
-        break;
-      }
-    } while(!blockValid);
-
-  })
-}
-
-
-const getBlockIndexes = async (blocks, currentBlock) => {
-  const findBlockRequests = [];
-  blocks.map(block => {
-    findBlockRequests.push(findValidBlock(block.addressesPerBlock, currentBlock));
-  });
-  return Promise.all(findBlockRequests);
-}
-
-const getAddressBlocks = async (numberOfAddresses, network) => {
-  return new Promise(async (resolve, reject) => {
-    const currentBlock = await provider.getBlockNumber();
-    let numberOfBlocks = getNumberOfBlocks(numberOfAddresses);
-    
-    console.log('Number of addresses and blocks', numberOfAddresses, numberOfBlocks);
-    
-    // This should be a matrix, in which each block would have at least one address
-    let blocks = getAddressesPerBlocks(numberOfAddresses, numberOfBlocks);
-
-    // Now i have to select block inedxes for each block
-    // The block must have enough transactions
-    const blockIndexes = await getBlockIndexes(blocks, currentBlock);
-    blocks = mergeArrayToCollection(blockIndexes, blocks, 'blockIndex');
-
-    console.log('blockIndexes', blockIndexes);
-
-    return resolve(blocks);
-  
-  })
-
-}
-
-let provider;
-const getTransaction = (currentBlock, blocksTreshold) => {
-  return new Promise(resolve => {
-    
-    const findTransaction = async () => {
-      const blockIndex = Math.floor(Math.random() * blocksTreshold);
-      const blockData = await provider.getBlock(currentBlock - blockIndex);
-      
-      if(blockData.transactions.length > 0) {
-        console.log('Block index', blockIndex);
-        const block = await provider.getBlock(currentBlock - blockIndex, true);
-        console.log(block.transactions);
-        const transaction = await provider.getTransaction(blockData.transactions[Math.floor(Math.random() * blockData.transactions.length)])
-        resolve(transaction.to || transaction.from);
-      }else{
-        findTransaction()
-      }
-    }
-
-    return findTransaction();
-
-  })
-}
-const getRandomAddresses = numberOfAddresses => {
-  // Get Random block number
-  
-  return new Promise(async (resolve, reject) => {
-    const currentBlock = await provider.getBlockNumber();
-    const response = [];
-    for(let i = 0; i < numberOfAddresses; i++) {
-      const tx = await getTransaction(currentBlock, blocksTreshold);
-      response.push(tx);
-    }
-    resolve(response);
-
-  })
-}
-
-
-
-
 const getValidBlock = async () => {
   return new Promise(async resolve => {
     const currentBlock = await provider.getBlockNumber();
@@ -212,29 +67,15 @@ const getAddresses =  async (numberOfAddresses = defaultNumberOfAddresses, netwo
   }
 
   provider = getProvider(network);
-
-  
-  //const addressBlocks = await getAddressBlocks(numberOfAddresses);
-  console.log('Starting');
-  //const addresses = await getRandomAddresses(numberOfAddresses);
-  const numberOfBlocks = getNumberOfBlocks(numberOfAddresses);
   const result = await search(numberOfAddresses);
   console.log(result);
-  //const block = await getValidBlock();
-  //console.log(block);
-  // for each block find appropriate number of transactions
-  console.log('Number of Blocks', numberOfBlocks);
-  //console.log(addresses);
-  
-  return '';
+  return result;
 }
 
-getAddresses(22, 'ropsten');
+
+getAddresses(122, 'ropsten');
 
 module.exports = {
-  getAddressBlocks,
-  getAddresses,
-  mergeArrayToCollection,
-  getNumberOfBlocks
+  getAddresses
 }
 
--- tests/index.js
+++ tests/index.js
@@ -2,47 +2,47 @@ const randomAddressGenerator = require('../src/index');
 const estimatedNumberOfTransactionsPerBlock = 10;
 
 
-describe('Generating number of addresses per block',  async () => {
-  test('Number of blocks', async () => {
-    const numberOfAddresses = 14;
-    const blocks = await randomAddressGenerator.getAddressBlocks(numberOfAddresses, 'ropsten');
-    const numberOfAddressesInResponse = blocks.reduce((curr, next) => {
-      return curr + next.addressesPerBlock
-    }, 0);
+// describe('Generating number of addresses per block',  async () => {
+//   test('Number of blocks', async () => {
+//     const numberOfAddresses = 14;
+//     const blocks = await randomAddressGenerator.getAddressBlocks(numberOfAddresses, 'ropsten');
+//     const numberOfAddressesInResponse = blocks.reduce((curr, next) => {
+//       return curr + next.addressesPerBlock
+//     }, 0);
 
-    expect(numberOfAddressesInResponse).toBe(numberOfAddresses);
-  })
-});
-
-
-
-describe('Merge array to collection', () => {
-  test('Basic functionality', () => {
-    const arr = ['1', '2', '3', '4'];
-    const coll = [{a: 'test'}, {b: 'test'}, {c: 'test'}];
-    const result = randomAddressGenerator.mergeArrayToCollection(arr, coll, 'test');
-    console.log(result);
-    expect(result.length).toBe(3);  
-  })
-})
+//     expect(numberOfAddressesInResponse).toBe(numberOfAddresses);
+//   })
+// });
 
 
-describe('Choosing number of blocks logic', () => {
-  test('Number of blocks', () => {
-    let numberOfAddresses = 133;
-    let numberOfBlocks = randomAddressGenerator.getNumberOfBlocks(numberOfAddresses);
-    expect(numberOfBlocks).toBe(Math.ceil(numberOfAddresses / estimatedNumberOfTransactionsPerBlock));
+
+// describe('Merge array to collection', () => {
+//   test('Basic functionality', () => {
+//     const arr = ['1', '2', '3', '4'];
+//     const coll = [{a: 'test'}, {b: 'test'}, {c: 'test'}];
+//     const result = randomAddressGenerator.mergeArrayToCollection(arr, coll, 'test');
+//     console.log(result);
+//     expect(result.length).toBe(3);  
+//   })
+// })
+
+
+// describe('Choosing number of blocks logic', () => {
+//   test('Number of blocks', () => {
+//     let numberOfAddresses = 133;
+//     let numberOfBlocks = randomAddressGenerator.getNumberOfBlocks(numberOfAddresses);
+//     expect(numberOfBlocks).toBe(Math.ceil(numberOfAddresses / estimatedNumberOfTransactionsPerBlock));
     
-    numberOfAddresses = 1;
-    numberOfBlocks = randomAddressGenerator.getNumberOfBlocks(numberOfAddresses);
-    expect(numberOfBlocks).toBe(1);
+//     numberOfAddresses = 1;
+//     numberOfBlocks = randomAddressGenerator.getNumberOfBlocks(numberOfAddresses);
+//     expect(numberOfBlocks).toBe(1);
     
-    numberOfAddresses = 8;
-    numberOfBlocks = randomAddressGenerator.getNumberOfBlocks(numberOfAddresses);
-    expect(numberOfBlocks).toBe(1);
+//     numberOfAddresses = 8;
+//     numberOfBlocks = randomAddressGenerator.getNumberOfBlocks(numberOfAddresses);
+//     expect(numberOfBlocks).toBe(1);
 
-  })
-})
+//   })
+// })
 
 
 
